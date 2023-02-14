d60b9e400f4cf5dd1e9f50d7c3b49e5c73dca001
12 <57660048+leviluo527@users.noreply.github.com>
2023-02-14 13:06:39 -0500

    2023.2.14

---
+++ 1 Two Sum.java
@@ -0,0 +1,40 @@
+/*Solution1: Use a hashmap to find the result
+* Time: O(n)
+* Space: O(n)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            //Since we don't allow an element to use twice
+            //we need to check if 2 * nums[i] = target
+            if (map.containsKey(remain) && map.get(remain) != i) {
+                return new int[]{i, map.get(remain)};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
+
+/*Solution2: Use one pass HashMap, everytime insert a value, we will look back
+* Time: O(n)
+* Space: O(n)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
