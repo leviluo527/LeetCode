34cecf912fd5b01610130da3cdfb77faf041033d
12 <57660048+leviluo527@users.noreply.github.com>
2023-02-16 19:27:37 -0500

    DP thought

---
+++ 42 Trapping Rain Water.java
@@ -0,0 +1,32 @@
+/*Solution: Use the trick of dp thought
+* Time: O(n)
+* Space: O(1)
+*/
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int res = 0;
+        while (left < right) {
+            //if left < right, we will move left
+            if (height[left] < height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                } else {
+                    res += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                } else {
+                    res += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}
