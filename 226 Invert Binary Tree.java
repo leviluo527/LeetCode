f4affe79ccfe2d7894cb5b1249b0cdec4c4b2275
12 <57660048+leviluo527@users.noreply.github.com>
2023-02-18 16:53:10 -0500

    Recursion

---
+++ 226 Invert Binary Tree.java
@@ -0,0 +1,16 @@
+/*Solution: Use recursion
+* Time: O(N)
+* Space: O(Height)
+*/
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
