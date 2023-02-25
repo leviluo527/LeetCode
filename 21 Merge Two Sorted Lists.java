71e4cd33e05a1adce0bb04530ae8b8f27972f2de
12 <57660048+leviluo527@users.noreply.github.com>
2023-02-24 20:31:03 -0500

    HashMap

---
+++ 21 Merge Two Sorted Lists.java
@@ -0,0 +1,27 @@
+/*Solution: Use a dummy node
+* Time: O(list1.length + list2.length)
+* Space: O(list1.length + list2.length)
+*/
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return dummy.next;
+    }
+}
