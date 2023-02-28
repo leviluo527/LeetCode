95c6c415925f0e2b7f4843445a89bd219b80e5ab
12 <57660048+leviluo527@users.noreply.github.com>
2023-02-27 21:52:01 -0500

    Recursion OR Iterative

--- 24 Swap Nodes in Pairs.java
+++ 24 Swap Nodes in Pairs.java
@@ -1,4 +1,4 @@
-/*Solution: Use recursion
+/*Solution1: Use recursion
 * Time: O(N)
 * Space: O(N / 2)
 */
@@ -12,5 +12,40 @@ class Solution {
         cur.next = swapPairs(next.next);
         next.next = cur;
         return next;
+    }
+}
+
+/*Solution2: Iteratve
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while ((head != null) && (head.next != null)) {
+
+            // Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+
+        // Return the new head node.
+        return dummy.next;
     }
 }
