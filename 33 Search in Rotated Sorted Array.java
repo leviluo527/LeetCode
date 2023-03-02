4f0cc825558e68e94dda53d36a67eb665b588232
12 <57660048+leviluo527@users.noreply.github.com>
2023-03-01 23:37:09 -0500

    Binary Search

---
+++ 33 Search in Rotated Sorted Array.java
@@ -0,0 +1,29 @@
+/*Solution: Use Binary Search + each time compare nums[mid] to nums[left] or nums[right]
+* Time: O(logn)
+* Space: O(1)
+*/
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] >= nums[l]) {
+                if (target < nums[m] && target >= nums[l]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
