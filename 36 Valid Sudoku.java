1f5fd5a90f72949d1363417c3b817979c1f5753b
12 <57660048+leviluo527@users.noreply.github.com>
2023-03-01 23:27:21 -0500

    HashSet

---
+++ 36 Valid Sudoku.java
@@ -0,0 +1,44 @@
+/*Solution: Use HashSet OR use int[][] as hashSet
+* Time: O(n^2)
+* Space: O(n^2)
+*/
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+
+        // Use an array to record the status
+        int[][] rows = new int[N][N];
+        int[][] cols = new int[N][N];
+        int[][] boxes = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                // Check if the position is filled with number
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                int pos = board[r][c] - '1';
+
+                // Check the row
+                if (rows[r][pos] == 1) {
+                    return false;
+                }
+                rows[r][pos] = 1;
+
+                // Check the column
+                if (cols[c][pos] == 1) {
+                    return false;
+                }
+                cols[c][pos] = 1;
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx][pos] == 1) {
+                    return false;
+                }
+                boxes[idx][pos] = 1;
+            }
+        }
+        return true;
+    }
+}
