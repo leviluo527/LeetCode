54e85b937d2c941eefcc4ea49378abef38ff7ddb
12 <57660048+leviluo527@users.noreply.github.com>
2023-03-08 20:28:29 -0500

    DP

---
+++ 63 Unique Paths II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length - 1;
+        int n = obstacleGrid[0].length - 1;
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        obstacleGrid[0][0] = 1;
+        //this case is for the edges, if last is an obstacle, next will also be 0
+        for (int i = 1; i <= m; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+        for (int j = 1; j <= n; j++) {
+            obstacleGrid[0][j] = (obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1] == 1) ? 1 : 0;
+        }
+        
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            for (int j = 1; j < obstacleGrid[0].length; j++) {
+                obstacleGrid[i][j] = obstacleGrid[i][j] == 0 ? obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1] : 0;
+            }
+        }
+        return obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+}
