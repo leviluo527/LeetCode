7cce6d4b16762e54cbaebe409ac9c761a5c50536
12 <57660048+leviluo527@users.noreply.github.com>
2023-03-31 15:37:39 -0700

    Rainbow sort

---
+++ 75 Sort Colors.java
@@ -0,0 +1,29 @@
+/*Solution: Rainbow sort, divide the array in three parts
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public void sortColors(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int k = nums.length - 1;
+        while (j <= k) {
+            if (nums[j] == 0) {
+                swap(nums, i, j);
+                i++;
+                j++;
+            } else if (nums[j] == 1) {
+                j++;
+            } else {
+                swap(nums, j, k);
+                k--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
