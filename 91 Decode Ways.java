82dea004606daed4ba1ada20fafb31853c188c35
12 <57660048+leviluo527@users.noreply.github.com>
2023-04-10 19:07:54 -0400

    DP

---
+++ 91 Decode Ways.java
@@ -0,0 +1,27 @@
+/*Solution: DP
+* Time: O(s.length())
+* Space: O(s.length())
+*/
+class Solution {
+
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for(int i = 2; i < dp.length; i++) {
+            // Check if successful single digit decode is possible.
+            if (s.charAt(i - 1) != '0') {
+               dp[i] = dp[i - 1];  
+            }
+            
+            // Check if successful two digit decode is possible.
+            int twoDigit = Integer.valueOf(s.substring(i - 2, i));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
