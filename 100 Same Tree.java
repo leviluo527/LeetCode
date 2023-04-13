b15eda3440c72e0c0ba30aa726a185cdcfb62002
12 <57660048+leviluo527@users.noreply.github.com>
2023-04-12 21:31:19 -0400

    Tree Recursion

---
+++ 100 Same Tree.java
@@ -0,0 +1,16 @@
+/*Solution: Tree Recursion
+* Time: O(N)
+* Space: O(LogN)
+*/
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
