06f3d80d5a749ddf4cd4932fa8f2d4d632f06b8b
12 <57660048+leviluo527@users.noreply.github.com>
2023-04-12 21:20:34 -0400

    Math

---
+++ 96 Unique Binary Search Trees.java
@@ -0,0 +1,14 @@
+/*Solution: Math
+* Time: O(n)
+* Space: O(1)
+*/
+class Solution {
+  public int numTrees(int n) {
+    // Note: we should use long here instead of int, otherwise overflow
+    long C = 1;
+    for (int i = 0; i < n; ++i) {
+      C = C * 2 * (2 * i + 1) / (i + 2);
+    }
+    return (int) C;
+  }
+}
