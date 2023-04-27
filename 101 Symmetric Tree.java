543cb5013246bae0b1df62930ec9211be8aadafa
12 <57660048+leviluo527@users.noreply.github.com>
2023-04-26 21:12:59 -0400

    recursion

---
+++ 101 Symmetric Tree.java
@@ -0,0 +1,25 @@
+/*Solution: Tree Recursion
+* Time: O(N)
+* Space: O(Height)
+*/
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
