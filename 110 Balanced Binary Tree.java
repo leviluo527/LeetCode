adc76e56c3739912c2c7eb1dc6f68d142fced756
12 <57660048+leviluo527@users.noreply.github.com>
2023-06-02 14:22:36 -0400

    Recursion

---
+++ 110 Balanced Binary Tree.java
@@ -0,0 +1,22 @@
+/*Solution: Recursion
+* Time: O(nlogn)
+* Space: O(logn)
+*/
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
