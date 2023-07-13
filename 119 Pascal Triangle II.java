ecd3b38e684a756af9c7cd3d8919c84069e3b05a
12 <57660048+leviluo527@users.noreply.github.com>
2023-07-13 16:02:00 -0400

    Math

---
+++ 119 Pascal Triangle II.java
@@ -0,0 +1,19 @@
+/*Solution: Use Math
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public List<Integer> getRow(int n) {
+        List<Integer> row = new ArrayList<>() {
+            {
+                add(1);
+            }
+        };
+
+        for (int k = 1; k <= n; k++) {
+            row.add((int) ((row.get(row.size() - 1) * (long) (n - k + 1)) / k));
+        }
+
+        return row;
+    }
+}
