ab0d37b21b8a8f461099d928cc6d7f089829ff17
12 <57660048+leviluo527@users.noreply.github.com>
2023-07-25 11:36:25 -0400

    HashSet or Two Pointer

---
+++ 142 Linked List Cycle II.java
@@ -0,0 +1,60 @@
+/*Solution1: Use HashSet
+* Time: O(N)
+* Space: O(N)
+*/
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null) {
+            if (set.contains(cur)) {
+                return cur;
+            } else {
+                set.add(cur);
+                cur = cur.next;
+            }
+        }
+        return null;
+    }
+}
+
+/*Solution2: Use slow fast pointer
+* Time: O(N)
+* Space: O(1)
+*/
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // Initialize tortoise and hare pointers
+        ListNode tortoise = head;
+        ListNode hare = head;
+
+        // Move tortoise one step and hare two steps
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+
+            // Check if the hare meets the tortoise
+            if (tortoise == hare) {
+                break;
+            }
+        }
+
+        // Check if there is no cycle
+        if (hare == null || hare.next == null) {
+            return null;
+        }
+
+        // Reset either tortoise or hare pointer to the head
+        hare = head;
+
+        // Move both pointers one step until they meet again
+        while (tortoise != hare) {
+            tortoise = tortoise.next;
+            hare = hare.next;
+        }
+
+        // Return the node where the cycle begins
+        return tortoise;
+    }
+}
