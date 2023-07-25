69951ace971383cde2208400a8b17d9a8c766d38
12 <57660048+leviluo527@users.noreply.github.com>
2023-07-25 11:38:30 -0400

    Two Pointer

---
+++ 141 Linked List Cycle.java
@@ -0,0 +1,21 @@
+/*Solution: Two Pointer
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
