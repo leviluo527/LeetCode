9378bf7372853e21b5b7107d2aeee201f19a7338
12 <57660048+leviluo527@users.noreply.github.com>
2023-07-27 16:46:03 -0400

    Recursion + Tree

---
+++ 156 Binary Tree Upside Down.java
@@ -0,0 +1,17 @@
+/*Solution: Save thought as recusive LinkedList
+* Time: O(n)
+* Space: O(logn)
+*/
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
