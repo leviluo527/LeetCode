cd8b87a2286c69d99c1d40ae549f518e64d41c52
Levi <57660048+leviluo527@users.noreply.github.com>
2023-07-31 11:39:58 -0400

    Binary Search

---
+++ 162 Find Peak Element.java
@@ -0,0 +1,19 @@
+/*Solution: Binary Search
+* Time: O(logn)
+* Space: O(1)
+*/
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
