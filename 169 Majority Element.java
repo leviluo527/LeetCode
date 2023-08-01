7c7f03cffef3682832ec16b476006c63bfc7bc09
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-01 10:19:29 -0400

    DP

---
+++ 169 Majority Element.java
@@ -0,0 +1,19 @@
+/*Solution: Usea a candidate to check each element in the array
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
