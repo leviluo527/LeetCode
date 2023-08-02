38df5dece48bee094d2b9a945c67251e17e8be0b
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-02 16:24:57 -0400

    Bit Manipulate

---
+++ 191 Number of 1 Bits.java
@@ -0,0 +1,15 @@
+/*Solution: Bit manipulation, bit setter
+* Time: O(32)
+* Space: O(1)
+*/
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1;
+        }
+        return count;
+    }
+}
