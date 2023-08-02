fa96c3a7d8ca9daacb3728a837f9dc094261cf6b
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-02 17:27:58 -0400

    Two Pointer

---
+++ 189 Rotate Array.java
@@ -0,0 +1,24 @@
+/*Solution: Reverse + reverse two pointer
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        if (k == 0) {
+            return;
+        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
