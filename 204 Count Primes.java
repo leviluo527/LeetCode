5c3c796eedac6028228ccc9f8e07369533d23fae
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-03 16:52:21 -0400

    Math

---
+++ 204 Count Primes.java
@@ -0,0 +1,29 @@
+/*Solution: Math, use an array to check
+* Time: O(n^-2 log(n))
+* Space: O(n)
+*/
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+        
+        boolean[] numbers = new boolean[n];
+        for (int p = 2; p <= (int)Math.sqrt(n); ++p) {
+            if (numbers[p] == false) {
+                for (int j = p*p; j < n; j += p) {
+                    numbers[j] = true;
+                }
+            }
+        }
+        
+        int numberOfPrimes = 0;
+        for (int i = 2; i < n; i++) {
+            if (numbers[i] == false) {
+                ++numberOfPrimes;
+            }
+        }
+        
+        return numberOfPrimes;
+    }
+}
