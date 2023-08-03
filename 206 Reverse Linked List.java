7cce541b743d01c0b58417793231365d4e533e51
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-03 16:06:55 -0400

    Recursion or Iterative

---
+++ 206 Reverse Linked List.java
@@ -0,0 +1,36 @@
+/*Solution1: Recursion
+* Time: O(N)
+* Space: O(N)
+*/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+/*Solution2: Iterative
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
