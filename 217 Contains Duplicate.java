51b46e5f887891db228f30382df3d39001057680
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-07 13:40:03 -0400

    HashSet

---
+++ 217 Contains Duplicate.java
@@ -0,0 +1,16 @@
+/*Solution: Use HashSet
+* Time: O(N)
+* Space: O(N)
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
