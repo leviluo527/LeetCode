e969057deb2c72725d61f3d8947263728b6a5d30
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-22 17:23:48 -0400

    Easy

---
+++ 263 Ugly Number.java
@@ -0,0 +1,24 @@
+/*Solution: Easy
+* Time: O(log5(n) * log2(n) * log3(n))
+* Space: O(1)
+*/
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
