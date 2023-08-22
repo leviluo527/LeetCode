0f92deeae119a9b91eb7562711030986f4ceadca
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-22 17:41:43 -0400

    DP

---
+++ 265 Paint House II.java
@@ -0,0 +1,44 @@
+/*Solution: 2D DP, remeber the first smallest index and second smallest index
+* Time: O(NK)
+* Space: O(1)
+*/
+class Solution {
+
+    public int minCostII(int[][] costs) {
+
+        if (costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+
+        for (int house = 1; house < n; house++) {
+
+            // Find the minimum and second minimum color in the PREVIOUS row.
+            int minColor = -1; int secondMinColor = -1;
+            for (int color = 0; color < k; color++) {
+                int cost = costs[house - 1][color];
+                if (minColor == -1 || cost < costs[house - 1][minColor]) {
+                    secondMinColor = minColor;
+                    minColor = color;
+                } else if (secondMinColor == -1 || cost < costs[house - 1][secondMinColor]) {
+                    secondMinColor = color;
+                }
+            }
+
+            // And now calculate the new costs for the current row.
+            for (int color = 0; color < k; color++) {
+                if (color == minColor) {
+                    costs[house][color] += costs[house - 1][secondMinColor];
+                } else {
+                    costs[house][color] += costs[house - 1][minColor];
+                }
+            }
+        }
+
+        // Find the minimum in the last row.
+        int min = Integer.MAX_VALUE;
+        for (int c : costs[n - 1]) {
+            min = Math.min(min, c);
+        }
+        return min;
+    }
+}
