f6444b23b08c4be279a02114ca08eaab2bff50ab
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-22 17:43:46 -0400

    ??

---
+++ 269 Alien Dictionary.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    private Map<Character, List<Character>> reverseAdjList = new HashMap<>();
+    private Map<Character, Boolean> seen = new HashMap<>();
+    private StringBuilder output = new StringBuilder();
+    
+    public String alienOrder(String[] words) {
+        
+        // Step 0: Put all unique letters into reverseAdjList as keys.
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                reverseAdjList.putIfAbsent(c, new ArrayList<>());
+            }
+        }
+        
+        // Step 1: Find all edges and add reverse edges to reverseAdjList.
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            // Check that word2 is not a prefix of word1.
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
+            // Find the first non match and insert the corresponding relation.
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    reverseAdjList.get(word2.charAt(j)).add(word1.charAt(j));
+                    break;
+                }
+            }
+        }
+        
+        // Step 2: DFS to build up the output list.
+        for (Character c : reverseAdjList.keySet()) {
+            boolean result = dfs(c);
+            if (!result) return "";
+        }
+        
+        return output.toString();
+    }
+    
+    // Return true iff no cycles detected.
+    private boolean dfs(Character c) {
+        if (seen.containsKey(c)) {
+            return seen.get(c); // If this node was grey (false), a cycle was detected.
+        }
+        seen.put(c, false);
+        for (Character next : reverseAdjList.get(c)) {
+            boolean result = dfs(next);
+            if (!result) return false;
+        }
+        seen.put(c, true);
+        output.append(c);
+        return true;
+    }    
+}
