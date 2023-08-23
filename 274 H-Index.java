3248182757a44a253bedc00a8864ce483ba3a1a0
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-23 19:38:36 -0400

    DP

---
+++ 274 H-Index.java
@@ -0,0 +1,18 @@
+/*Solution: DP
+* Time: O(N)
+* Space: O(N)
+*/
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // finding the h-index
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
+    }
+}
