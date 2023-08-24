351a7a32b5e7d2d8168c3ba12fe08a453c16d976
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-24 19:51:57 -0400

    Greedy

---
+++ 277 Find the Celebrity.java
@@ -0,0 +1,34 @@
+/*Solution: First, we scan from 0 to n, to find the people that may not know every one
+* Time: O(N)
+* Space: O(1)
+*/
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    private int numberOfPeople;
+        
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
+            }
+        }
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
+        return -1;
+    }
+        
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
