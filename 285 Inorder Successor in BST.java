ac200832e65f98d3b7e0c1c037a7139f1b97a0af
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-25 20:14:55 -0400

    Tree

---
+++ 285 Inorder Successor in BST.java
@@ -0,0 +1,23 @@
+/*Solution: Tree Recursive
+* Time: O(Height)
+* Space: O(1)
+*/
+class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        
+        TreeNode successor = null;
+        
+        while (root != null) {
+            
+            if (p.val >= root.val) {
+                root = root.right;
+            } else {
+                successor = root;
+                root = root.left;
+            }
+        }
+        
+        return successor;
+    }
+}
