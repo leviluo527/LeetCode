df5bc0bf009b6b6828288228fcc48f4af91a631c
Levi <57660048+leviluo527@users.noreply.github.com>
2023-08-29 18:57:11 -0400

    DP

---
+++ 309 Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,20 @@
+/*Solution: DP
+* Time: O(N)
+* Space: O(1)
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+  
+      int sold = Integer.MIN_VALUE, held = Integer.MIN_VALUE, reset = 0;
+  
+      for (int price : prices) {
+        int preSold = sold;
+  
+        sold = held + price;
+        held = Math.max(held, reset - price);
+        reset = Math.max(reset, preSold);
+      }
+  
+      return Math.max(sold, reset);
+    }
+  }
